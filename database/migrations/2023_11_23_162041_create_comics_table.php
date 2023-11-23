75aa381f149153a9ebdd0234bab6be6cdf724b0d
FraRussotto <russottofrancesco1@gmail.com>
2023-11-23 17:56:04 +0100

    [edit] migrate comics

---
+++ database/migrations/2023_11_23_162041_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('slug', 52);
+            $table->text('description');
+            $table->string('thumb', 400);
+            $table->string('price', 10);
+            $table->string('serie', 100);
+            $table->string('sale_date', 10);
+            $table->string('type', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
