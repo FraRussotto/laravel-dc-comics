ce4543c482cfaf489e1b49ced4d237aeacd8766a
FraRussotto <russottofrancesco1@gmail.com>
2023-11-23 18:38:07 +0100

    [edit] comic seeder

--- database/migrations/2023_11_23_162041_create_comics_table.php
+++ database/migrations/2023_11_23_162041_create_comics_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50);
-            $table->string('slug', 52);
+            $table->string('title', 100);
+            // $table->string('slug', 102)->unique()->nullable();
             $table->text('description');
             $table->string('thumb', 400);
             $table->string('price', 10);
-            $table->string('serie', 100);
+            $table->string('series', 100);
             $table->string('sale_date', 10);
             $table->string('type', 20);
             $table->timestamps();
